b8313e8286f32f1a111ce0c5d99c5dfcf8a659bd
Denilson Oliveira <89954041+denilsude@users.noreply.github.com>
2022-11-30 21:11:29 -0300

    Add files via upload

---
+++ Login/meu_primeiro_codigocsharp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        string UsuarioSistema = "usuario";
+        string SenhaSistema = "123";
+
+        Console.WriteLine("Usuario: " );
+        string UsuarioInserido = Console.ReadLine();
+        Console.WriteLine();
+
+        Console.Write("senha: ");
+        String SenhaInserida = Console.ReadLine();
+        Console.WriteLine();
+
+        if (UsuarioInserido != UsuarioSistema || SenhaInserida != SenhaSistema)
+            Console.WriteLine("O usuario ou sua  senha esta incorreta");
+        else
+        Console.WriteLine("Bem vindo ao sistema");
+
+
+
+
+
+
+
+
+
+
+
+
+              Console.ReadKey();
+
+    }
+    }
+
